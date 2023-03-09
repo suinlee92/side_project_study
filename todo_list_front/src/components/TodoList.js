0f98881fce8b8241752b878080a0f92b868ce75d
suinlee92 <dltndls4535@gmail.com>
2023-03-09 22:53:38 +0900

    select add

--- todo_list_front/src/components/TodoList.js
+++ todo_list_front/src/components/TodoList.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import TodoItem from './TodoItem';
 import { useTodoState } from '../TodoContext';
+import axios from 'axios';
 
 const TodoListBlock = styled.div`
   flex: 1;
@@ -10,17 +11,26 @@ const TodoListBlock = styled.div`
   overflow-y: auto;
 `;
 
+
 function TodoList() {
-  const todos = useTodoState();
+  const [todos, setTodos] = useState([]);
+  
+  useEffect(()=> {
+    const fetchTodolist = async()=> {
+      const todolist = await axios.get('http://localhost:8080/todolist');
+      setTodos(todolist.data);
+    }
+    fetchTodolist();
+  }, []);
 
   return (
     <TodoListBlock>
       {todos.map(todo => (
         <TodoItem
-          key={todo.id}
-          id={todo.id}
-          text={todo.text}
-          done={todo.done}
+          key={todo.TODOLIST_MASTER_ID}
+          id={todo.TODOLIST_MASTER_ID}
+          text={todo.MASTER_CONTENTS}
+          done={todo.TODOLIST_STATUS}
         />
       ))}
     </TodoListBlock>
